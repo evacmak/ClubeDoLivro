5999048bdaedd9617b64221734a43241d5927d96
“Eva <evacmak@outlook.pt>
2024-06-20 21:51:31 +0100

    4 dia

--- src/pages/Cicatriz.jsx
+++ src/pages/Cicatriz.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { px } from "framer-motion";
+
 
 const Cicatriz = () => {
   const [book, setBook] = useState(null);
@@ -11,10 +13,8 @@ const Cicatriz = () => {
       const response = await axios.get(
         "https://www.googleapis.com/books/v1/volumes?q=maria%20francisca%20gama"
       );
-      console.log(response.data);
       setBook(response.data);
     } catch (error) {
-      console.error("Error fetching the books", error);
       setError("Error fetching the books");
     } finally {
       setLoading(false);
@@ -26,28 +26,43 @@ const Cicatriz = () => {
   }, []);
 
   if (loading) {
-    return <p>Loading...</p>;
+    return <p className="loading">Loading...</p>;
   }
 
   if (error) {
-    return <p>{error}</p>;
+    return <p className="error">{error}</p>;
   }
 
   if (!book || !book.items || book.items.length === 0) {
-    return <p>No books found</p>;
+    return <p className="no-books">No books found</p>;
   }
 
   const bookData = book.items[0].volumeInfo;
 
   return (
-    <div>
-      <img
-        src={bookData.imageLinks?.thumbnail || 'https://via.placeholder.com/200'}
-        alt={bookData.title || 'No title available'}
-        style={{ width: '200px', height: 'auto' }}
-      />
-      <h1>{bookData.title || 'No title available'}</h1>
-      <h2>{bookData.authors?.join(', ') || 'No authors available'}</h2>
+    <div className="container">
+      <div className="book-content">
+      <div className="book-image">
+        <img
+          src={bookData.imageLinks?.thumbnail}
+          alt={bookData.title}
+          width={'1000px'}
+        />
+          <a href={bookData.infoLink} className="button" target="_blank">
+            Comprar o livro
+          </a>
+      </div>
+        <div className="details">
+          <h1 className="book-title">{bookData.title || 'No title available'}</h1>
+          <h2 className="book-authors">{bookData.authors?.join(', ') || 'Não disponível'}</h2>
+          <p className="book-meta">{bookData.categories?.join(', ') || 'Não disponível'}</p>
+          <p className="book-meta">Publicado por: {bookData.publisher || 'Não disponível'}</p>
+          <p className="book-meta">Publicado em: {bookData.publishedDate || 'Não disponível'}</p>
+          <p className="book-description">{bookData.description || 'Não disponível'}</p>
+        </div>
+        
+        
+      </div>
     </div>
   );
 };
