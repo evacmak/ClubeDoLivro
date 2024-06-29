f119ba058e2782ee1ca6fa0be290bac5967bbb8b
“Eva <evacmak@outlook.pt>
2024-06-29 19:49:04 +0100

    a funcionar 19:49

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,12 +9,12 @@ const Navbar = () => {
     return (
         <nav className={`Navbar ${theme}`}>
             <NavLink to="/">
-                <img src={logo} alt="Clube Logo" className="nav-logo" style={{ width: "120px", height: "auto", marginLeft: "15px" }} />
+                <img src={logo} alt="Clube Logo" className="nav-logo" style={{width: "120px", height: "auto", marginLeft: "15px" }} />
             </NavLink>
             <ul>
-                <NavLink className={({ isActive }) => (isActive ? 'selected' : '')} to="/">Home</NavLink>
-                <NavLink className={({ isActive }) => (isActive ? 'selected' : '')} to="/livros">Livros</NavLink>
-                <NavLink className={({ isActive }) => (isActive ? 'selected' : '')} to="https://chat.whatsapp.com/H8MdpjbRn795Y2M6pMBhBA" target="_blank" style={{ backgroundColor: '#3526DE', padding: '5px 13px', color: 'white', fontWeight: '1000' }}>Aderir</NavLink>
+                <NavLink className={({ isActive }) => (isActive ? 'selected' : '')} to="/" style={{fontFamily: 'Bebas, sans-serif', color: '#333333', fontSize:'25px', fontWeight:'600'}}>Home</NavLink>
+                <NavLink className={({ isActive }) => (isActive ? 'selected' : '')} to="/livros" style={{fontFamily: 'Bebas, sans-serif', color: '#333333', fontSize:'25px', fontWeight:'600'}}>Livros</NavLink>
+                <NavLink className={({ isActive }) => (isActive ? 'selected' : '')} to="https://chat.whatsapp.com/H8MdpjbRn795Y2M6pMBhBA" target="_blank" style={{fontFamily: 'Bebas, sans-serif', fontSize:'25px', fontWeight:'500', backgroundColor: '#3526DE', padding: '5px 13px', color: 'white'}}>Aderir</NavLink>
             </ul>
         </nav>
     );
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,8 +3,7 @@ import Hero from "../components/Hero";
 import ThreeColumns from "../components/ThreeColumns";
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { Box, CardBody, Center, Image, Card, Text } from "@chakra-ui/react";
-import { NavLink } from "react-router-dom";
+import { Box, Center, Image, Text } from "@chakra-ui/react";
 import livroHarry from "../images/livro-harry.jpeg";
 import cookiesFoto from "../images/cookies.jpeg";
 import evaFoto from "../images/evaBookshop.jpeg";
@@ -34,162 +33,171 @@ const Homepage = () => {
 
   return (
     <div>
-
-    <div>
-    <h1 style={{ textAlign: 'center', marginTop: '2rem', fontSize: '80px', fontFamily: 'Bebas, sans-serif', color: '#333333' }}>Clube do Livro</h1>
-      <h1 style={{ textAlign: 'center', marginTop: '5px', fontSize: '30px', fontFamily: "Permanent Marker, cursive", color: "#3526DE", fontWeight: '1' }}>Eva & Marta</h1>
-    </div>
-    <Center>
-      <Box display='flex' justifyContent='center' alignItems='center' marginTop='40px'>
-        <Box
-          maxW='400px'
-          borderWidth='1px'
-          borderRadius='lg'
-          overflow='hidden'
-          m={2}
-          position='relative' // Set position to relative for absolute positioning inside
-          boxShadow='lg' // Add boxShadow here to apply a large shadow
-          textAlign='center' // Center align the text
-        >
-          <Image src={cookiesFoto} maxH='400px' />
-
-          <Text
-            position='absolute'
-            top='50%' // Center vertically
-            left='50%' // Center horizontally
-            transform='translate(-50%, -50%)' // Move text back by 50% of its width and height to center it
-            fontSize='xl' // Adjust font size as needed
-            fontWeight='bold' // Adjust font weight as needed
-            color='white' // Adjust text color as needed
-            zIndex='1' // Ensure text appears above the image
-            p={0} // Padding around the text
-            borderRadius='md' // Rounded corners for the background
-            fontFamily={"Lato, sans-serif"}
+      <div>
+        <h1 style={{ textAlign: 'center', marginTop: '2rem', fontSize: '100px', fontFamily: 'Bebas, sans-serif', color: '#333333' }}>Clube do Livro</h1>
+        <h1 style={{ textAlign: 'center', marginTop: '5px', fontSize: '30px', fontFamily: "Permanent Marker, cursive", color: "#3526DE", fontWeight: '1' }}>Eva & Marta</h1>
+      </div>
+      
+      <Center>
+        <Box display='flex' justifyContent='center' alignItems='center' marginTop='40px'>
+          <Box
+            maxW='400px'
+            borderWidth='1px'
+            borderRadius='lg'
+            overflow='hidden'
+            m={2}
+            position='relative'
+            boxShadow='lg'
+            textAlign='center'
+          >
+            <Image src={cookiesFoto} maxH='400px' />
+            <Text
+              position='absolute'
+              top='50%'
+              left='50%'
+              transform='translate(-50%, -50%)'
+              fontSize='xl'
+              fontWeight='bold'
+              color='white'
+              zIndex='1'
+              p={0}
+              borderRadius='md'
+              fontFamily={"Lato, sans-serif"}
+              textShadow='2px 2px 8px rgba(0, 0, 0, 0.8)'
+            >
+              Snacks & Books
+            </Text>
+          </Box>
+          
+          <Box
+            maxW='400px'
+            borderWidth='1px'
+            borderRadius='lg'
+            overflow='hidden'
+            m={2}
+            position='relative'
+            boxShadow='lg'
+            textAlign='center'
             textShadow='2px 2px 8px rgba(0, 0, 0, 0.8)'
           >
-           Snacks & Books
-           </Text>
-        </Box>
-        <Box
-          maxW='400px'
-          borderWidth='1px'
-          borderRadius='lg'
-          overflow='hidden'
-          m={2}
-          position='relative' // Set position to relative for absolute positioning inside
-          boxShadow='lg' // Add boxShadow here to apply a large shadow
-          textAlign='center' // Center align the text
-          textShadow='2px 2px 8px rgba(0, 0, 0, 0.8)'
-        >
-          <Image src={martaFoto} maxH='400px' />
+            <Image src={martaFoto} maxH='400px' />
+            <Text
+              position='absolute'
+              top='50%'
+              left='50%'
+              transform='translate(-50%, -50%)'
+              fontSize='xl'
+              fontWeight='bold'
+              color='white'
+              zIndex='1'
+              p={0}
+              borderRadius='md'
+              fontFamily={"Lato, sans-serif"}
+              textShadow='2px 2px 8px rgba(0, 0, 0, 0.8)'
+            >
+              Marta Santos
+            </Text>
+          </Box>
 
-          <Text
-            position='absolute'
-            top='50%' // Center vertically
-            left='50%' // Center horizontally
-            transform='translate(-50%, -50%)' // Move text back by 50% of its width and height to center it
-            fontSize='xl' // Adjust font size as needed
-            fontWeight='bold' // Adjust font weight as needed
-            color='white' // Adjust text color as needed
-            zIndex='1' // Ensure text appears above the image
-            p={0} // Padding around the text
-            borderRadius='md' // Rounded corners for the background
-            fontFamily={"Lato, sans-serif"}
-            textShadow='2px 2px 8px rgba(0, 0, 0, 0.8)'
+          <Box
+            maxW='400px'
+            borderWidth='1px'
+            borderRadius='lg'
+            overflow='hidden'
+            m={2}
+            position='relative'
+            boxShadow='lg'
+            textAlign='center'
           >
-            Marta Santos
-          </Text>
-        </Box>
+            <Image src={evaFoto} maxH='400px' />
+            <Text
+              position='absolute'
+              top='50%'
+              left='50%'
+              transform='translate(-50%, -50%)'
+              fontSize='xl'
+              fontWeight='bold'
+              color='white'
+              zIndex='1'
+              p={0}
+              borderRadius='md'
+              fontFamily={"Lato, sans-serif"}
+              textShadow='2px 2px 8px rgba(0, 0, 0, 0.8)'
+            >
+              Eva Cmak
+            </Text>
+          </Box>
 
-        <Box
-          maxW='400px'
-          borderWidth='1px'
-          borderRadius='lg'
-          overflow='hidden'
-          m={2}
-          position='relative' // Set position to relative for absolute positioning inside
-          boxShadow='lg' // Add boxShadow here to apply a large shadow
-          textAlign='center' // Center align the text
-        >
-          <Image src={evaFoto} maxH='400px' />
-
-          <Text
-            position='absolute'
-            top='50%' // Center vertically
-            left='50%' // Center horizontally
-            transform='translate(-50%, -50%)' // Move text back by 50% of its width and height to center it
-            fontSize='xl' // Adjust font size as needed
-            fontWeight='bold' // Adjust font weight as needed
-            color='white' // Adjust text color as needed
-            zIndex='1' // Ensure text appears above the image
-            p={0} // Padding around the text
-            borderRadius='md' // Rounded corners for the background
-            fontFamily={"Lato, sans-serif"}
-            textShadow='2px 2px 8px rgba(0, 0, 0, 0.8)'
+          <Box
+            maxW='400px'
+            borderWidth='1px'
+            borderRadius='lg'
+            overflow='hidden'
+            m={2}
+            position='relative'
+            boxShadow='lg'
+            textAlign='center'
           >
-            Eva Cmak
-          </Text>
+            <Image src={livroHarry} maxH='400px' />
+            <Text
+              position='absolute'
+              top='50%'
+              left='50%'
+              transform='translate(-50%, -50%)'
+              fontSize='xl'
+              fontWeight='bold'
+              color='white'
+              zIndex='1'
+              p={0}
+              borderRadius='md'
+              fontFamily={"Lato, sans-serif"}
+              textShadow='2px 2px 8px rgba(0, 0, 0, 0.8)'
+            >
+              Marcadores do Clube
+            </Text>
+          </Box>
         </Box>
-
-        <Box
-          maxW='400px'
-          borderWidth='1px'
-          borderRadius='lg'
-          overflow='hidden'
-          m={2}
-          position='relative' // Set position to relative for absolute positioning inside
-          boxShadow='lg' // Add boxShadow here to apply a large shadow
-          textAlign='center' // Center align the text
-        >
-          <Image src={livroHarry} maxH='400px' />
-
-          <Text
-            position='absolute'
-            top='50%' // Center vertically
-            left='50%' // Center horizontally
-            transform='translate(-50%, -50%)' // Move text back by 50% of its width and height to center it
-            fontSize='xl' // Adjust font size as needed
-            fontWeight='bold' // Adjust font weight as needed
-            color='white' // Adjust text color as needed
-            zIndex='1' // Ensure text appears above the image
-            p={0} // Padding around the text
-            borderRadius='md' // Rounded corners for the background
-            fontFamily={"Lato, sans-serif"}
-            textShadow='2px 2px 8px rgba(0, 0, 0, 0.8)'
-          >
-            Marcadores do Clube
-          </Text>
-        </Box>
-      </Box>
-    </Center>
+      </Center>
 
       <Hero />
       <ThreeColumns />
       <h1 style={{ textAlign: 'center', marginTop: '2rem' }}>Escolhas deste mês</h1>
 
-      {books.length > 0 && (
-        <div style={{ display: 'flex', justifyContent: 'center', gap: '50px', marginTop: '3rem', marginBottom: '3rem'}}>
-          {books[0]?.items[0] && (
-            <ImageCard
-              src={books[0].items[0].volumeInfo?.imageLinks?.thumbnail || ''}
-              header={books[0].items[0].volumeInfo?.title || 'No title available'}
-              meta={books[0].items[0].volumeInfo?.authors?.join(', ') || 'No authors available'}
-              extra='Romantasy 🔪❤️'
-            />
-          )}
-          {books[1]?.items[0] && (
-            <ImageCard
-              src={books[1].items[1].volumeInfo?.imageLinks?.thumbnail || ''}
-              header={books[1].items[0].volumeInfo?.title || 'No title available'}
-              meta={books[1].items[0].volumeInfo?.authors?.join(', ') || 'No authors available'}
-              extra='Romance 👩‍❤️‍👨'
-            />
-          )}
-        </div>
-      )}
+      <div>
+        {books.length > 0 && (
+          <div style={{ display: 'flex', justifyContent: 'center', gap: '50px', marginTop: '3rem', marginBottom: '3rem'}}>
+            {books[0]?.items[0] && (
+              <a href="https://clube-eva-marta.netlify.app/livro/assistant-to%20the%20villain-hannah-nicole%20maehrer" style={{ textDecoration: 'none' }}>
+                <ImageCard
+                  key={books[0].items[0].id} 
+                  src={books[0].items[0].volumeInfo?.imageLinks?.thumbnail || ''}
+                  header={books[0].items[0].volumeInfo?.title || 'No title available'}
+                  meta={books[0].items[0].volumeInfo?.authors?.join(', ') || 'No authors available'}
+                  extra='Romantasy 🔪❤️'
+                  style={{ width: '300px', height: '300px' }}
+                />
+              </a>
+            )}
+            {books[1]?.items[0] && (
+              <a href="https://clube-eva-marta.netlify.app/livro/love-redesigned-lauren-asher" style={{ textDecoration: 'none' }}>
+                <ImageCard
+                  key={books[1].items[0].id} 
+                  src={books[1].items[0].volumeInfo?.imageLinks?.thumbnail || ''}
+                  header={books[1].items[0].volumeInfo?.title || 'No title available'}
+                  meta={books[1].items[0].volumeInfo?.authors?.join(', ') || 'No authors available'}
+                  extra='Romance 👩‍❤️‍👨'
+                  style={{ width: '300px', height: '300px' }}
+                />
+              </a>
+            )}
+          </div>
+        )}
+      </div>
 
+
+      <div style={{ height: '100px' }}></div>
     </div>
   );
-  };
+};
 
 export default Homepage;
